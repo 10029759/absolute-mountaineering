8beaa7c4f86571a0dd4a50dd246085c57e8d759b
10029759 <joseph.andrews@hotmail.co.uk>
2022-08-10 18:36:50 +0100

    Update link

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@
 			<div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
 			  <div class="flex justify-start lg:w-0 lg:flex-1">
 				<a href="#">
-				  <span class="sr-only">Workflows</span>
+				  <span class="sr-only">Workflowsdsa</span>
 				  <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="">
 				</a>
 			  </div>
