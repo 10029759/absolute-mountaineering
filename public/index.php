ec6c95c48eb11be07ceb9e78776fbc1a08917220
10029759 <joseph.andrews@hotmail.co.uk>
2022-08-12 10:59:33 +0100

    Update the homepage to hold a little intro text

--- public/index.php
+++ public/index.php
@@ -262,9 +262,9 @@
 		  -->
 		  
 		  <div x-data="{ open: false }"
-		    x-show="open"
-		    @open-dropdown.window="if ($event.detail.id == 1) open = true"
-		    @click.away="open = false" class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+			x-show="open"
+			@open-dropdown.window="if ($event.detail.id == 1) open = true"
+			@click.away="open = false" class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
 			<div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
 			  <div class="pt-5 pb-6 px-5">
 				<div class="flex items-center justify-between">
@@ -272,7 +272,7 @@
 					<img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
 				  </div>
 				  <div class="-mr-2">
-				  	<!-- this is the close button for the mobile menu. The show/open is at the top of the page -->
+					<!-- this is the close button for the mobile menu. The show/open is at the top of the page -->
 					<button x-on:click="open = ! open" type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
 					  <span class="sr-only">Close menu</span>
 					  <!-- Heroicon name: outline/x -->
@@ -352,5 +352,74 @@
 		  </div> <!-- end of mobile menu -->
 		</div>
 
+		<!-- Main Content -->
+		<main>
+			<!-- This example requires Tailwind CSS v2.0+ -->
+			<div class="py-12 bg-white">
+			  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+				<div class="lg:text-center">
+				  <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Trips</h2>
+				  <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">A better experience...</p>
+				  <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">Here's what you should expect from join us on one of our courses.</p>
+				</div>
+
+				<div class="mt-10">
+				  <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+					<div class="relative">
+					  <dt>
+						<div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+						  <!-- Heroicon name: outline/globe-alt -->
+						  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+						  </svg>
+						</div>
+						<p class="ml-16 text-lg leading-6 font-medium text-gray-900">Competitive rates</p>
+					  </dt>
+					  <dd class="mt-2 ml-16 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+					</div>
+
+					<div class="relative">
+					  <dt>
+						<div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+						  <!-- Heroicon name: outline/scale -->
+						  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
+						  </svg>
+						</div>
+						<p class="ml-16 text-lg leading-6 font-medium text-gray-900">No hidden fees</p>
+					  </dt>
+					  <dd class="mt-2 ml-16 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+					</div>
+
+					<div class="relative">
+					  <dt>
+						<div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+						  <!-- Heroicon name: outline/lightning-bolt -->
+						  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M13 10V3L4 14h7v7l9-11h-7z" />
+						  </svg>
+						</div>
+						<p class="ml-16 text-lg leading-6 font-medium text-gray-900">Transfers are instant</p>
+					  </dt>
+					  <dd class="mt-2 ml-16 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+					</div>
+
+					<div class="relative">
+					  <dt>
+						<div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+						  <!-- Heroicon name: outline/annotation -->
+						  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+						  </svg>
+						</div>
+						<p class="ml-16 text-lg leading-6 font-medium text-gray-900">Mobile notifications</p>
+					  </dt>
+					  <dd class="mt-2 ml-16 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+					</div>
+				  </dl>
+				</div>
+			  </div>
+			</div>
+		</main>
 	</body>
 </html>
